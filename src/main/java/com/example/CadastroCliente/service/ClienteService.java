280fe38e66f8fdb8c975b75490cf9c08f20e46f8
Gabriel Kiyoto <120345849+Nagi1867@users.noreply.github.com>
2025-04-09 20:56:51 -0300

    service, resource

---
+++ src/main/java/com/example/CadastroCliente/service/ClienteService.java
@@ -0,0 +1,24 @@
+package com.example.CadastroCliente.service;
+
+import com.example.CadastroCliente.entitites.Cliente;
+import com.example.CadastroCliente.repositories.ClienteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ClienteService {
+    @Autowired
+    private ClienteRepository repository;
+
+    public List<Cliente> findAll() {
+        return repository.findAll();
+    }
+
+    public Cliente findById(Long id) {
+        Optional<Cliente> obj = repository.findById(id);
+        return obj.get();
+    }
+}
