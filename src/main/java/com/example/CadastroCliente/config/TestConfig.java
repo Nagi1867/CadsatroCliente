6d0264bb3b04be77307af004f49528c8df6d1395
Gabriel Kiyoto <120345849+Nagi1867@users.noreply.github.com>
2025-04-09 20:50:46 -0300

    injeção de dependencias

---
+++ src/main/java/com/example/CadastroCliente/config/TestConfig.java
@@ -0,0 +1,22 @@
+package com.example.CadastroCliente.config;
+
+import com.example.CadastroCliente.entitites.Cliente;
+import com.example.CadastroCliente.repositories.ClienteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Configuration
+@Profile("test")
+public class TestConfig implements CommandLineRunner {
+    @Autowired
+    private ClienteRepository repository;
+
+    @Override
+    public void run(String... args) throws Exception {
+        Cliente cliente = new Cliente(null, "Giuseppi", "40028922232", "inicial", 1);
+
+        repository.save(cliente);
+    }
+}
